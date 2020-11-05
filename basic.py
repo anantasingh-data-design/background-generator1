6814df87c6f8694c3520ba4e0ff42e7054871bca
anantasingh-data-design <anantkumarsingh05@gmail.com>
2020-11-05 19:59:27 +0530

    adding starting project you can check

---
+++ basic.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Hello world</h1>"
+
+@app.route('/information')
+def info():
+    return "<h1>puppies are cute!</h1>"
+
+@app.route('/puppy/<name>')
+def  puppy(name):
+    return "upper case: {}".format(name.upper())
+if __name__=='__main__':
+    app.run()
